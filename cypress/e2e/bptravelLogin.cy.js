cb10bfb510ee8c1802a81c397ee87565188ad18e
suryaraosdet <suryaraosdet@gmail.com>
2025-08-31 22:44:00 +0530

    Initial Cypress project commit

---
+++ cypress/e2e/bptravelLogin.cy.js
@@ -0,0 +1,10 @@
+describe('BP Travel Wesbite', () => {
+  it('Bp Travel Login', () => {
+    cy.visit('https://bptravel.blueprism.com/')
+    cy.get('#username').should('be.visible').click().type('admin');
+    cy.get('#password').should('be.visible').click().type('admin');
+    cy.get('span[onclick="login()"]').click();
+    cy.url().should('include','search.html');
+    cy.contains('Log Out').click();
+  })
+})
